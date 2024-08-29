9474e75061e7985efc2290387172132d788291db
Amanda Fahy <amandafahy@ymail.com>
2024-08-29 23:19:18 +0000

    Add basic template to homepage

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views import generic
+from .models import Post
 # Create your views here.
 
-def my_blog(request):
-    return HttpResponse("Hello, Blog!")
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'index.html'
+
+class PostDetail(generic.DetailView):
+    queryset = Post.objects.filter(status=1)
+    template_name = "post_list.html"
