c8df4f6200068d74e095da9071432d9852b3be30
Amanda Fahy <amandafahy@ymail.com>
2024-08-29 18:35:12 +0000

    Set up database , add sueruser

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-kop3tc64ci$)^9%%s1nai=k@@0w2snb-0+@*q9o8l4!&@7d0o1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-amandab2023-p4blog-ix3q1cf31u6.ws-eu115.gitpod.io','.herokuapp.com']
 
@@ -81,6 +85,9 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
